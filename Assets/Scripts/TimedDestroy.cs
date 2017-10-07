60205213351f6f2770b32be8417f7b03eaf60937
elninowang <elninowang@qq.com>
2017-10-07 14:24:59 +0800

    complete basic game， not complete

---
+++ Assets/Scripts/TimedDestroy.cs
@@ -0,0 +1,25 @@
+﻿//Programmed by Alan Thorn 2015
+//------------------------------
+using UnityEngine;
+using System.Collections;
+//------------------------------
+public class TimedDestroy : MonoBehaviour 
+{
+	public float DestroyTime = 2f;
+
+	//------------------------------
+	// Use this for initialization
+	void Start ()
+	{
+        Debug.Log("TimedDestroy::Start()");
+        Invoke("Die", DestroyTime);
+	}
+	
+	// Update is called once per frame
+	void Die () 
+	{
+		Destroy(gameObject);
+	}
+	//------------------------------
+}
+//------------------------------
